1f060aecbe966173bdc512a8ead19255733d92bc
Chitranshi1~ <chitranshigourana@gmail.com>
2024-02-27 18:11:09 +0530

    Second-Commit

--- src/Components/SearchCoinCard.jsx
+++ src/Components/SearchCoinCard.jsx
@@ -44,7 +44,7 @@ const SearchCoinCard = ({ coin }) => {
             width: "48%",
             height: "50%",
             borderRadius: "50%",
-            padding: "2rem"}}
+            padding: "2rem"s}}
             image={large ? large : NoImg }
           />
           <CardContent>
